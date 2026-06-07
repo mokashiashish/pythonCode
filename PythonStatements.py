24389934885fdd0f979d6474bfecb7cd67d9f0b3
mokashiashish <ashish.mokashi@gmail.com>
2026-06-07 12:29:09 -0500

    pything statements

---
+++ PythonStatements.py
@@ -0,0 +1,22 @@
+#if Elif 
+Hungry = True
+if Hungry == True:
+    print ("feed the dog")
+    
+
+else:
+    print ("dont feed the dog")
+
+# For  loop
+mylist = [1,2,3,4,5]
+for num in mylist:
+    if num % 2 == 0:
+        print (str(num) + " is even")
+    else:
+        print (str(num) + " is odd")
+
+
+
+
+#operarions
+
